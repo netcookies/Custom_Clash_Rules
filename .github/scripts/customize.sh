2000385aca1821faa5996a0c5def730504d07660
Isulew <netcookies@gmail.com>
2025-05-30 16:15:43 +0800

    fix

--- .github/scripts/customize.sh
+++ .github/scripts/customize.sh
@@ -13,7 +13,7 @@ find "$CFG_DIR" -type f -name "*.ini" | while read -r file; do
     sed -i '/^ruleset=🚀 手动选择,\[\]GEOSITE,gfw$/a ruleset=🌸 红杏影视,https://raw.githubusercontent.com/netcookies/Custom_Clash_Rules/main/rules/hxmovie.list,28800' "$file"
 
     # 第二处：在“设置分组标志位”注释上方插入自定义 proxy group
-    sed -i '/^;设置分组标志位$/i custom_proxy_group=🌸 红杏影视`url-test(红杏|红杏云|hongxingdl|hongxing|hongxingyun)`https://cp.cloudflare.com/generate_204300,,50' "$file"
+    sed -i '/^;设置分组标志位$/i custom_proxy_group=🌸 红杏影视`url-test`(红杏|红杏云|hongxingdl|hongxing|hongxingyun)`https://cp.cloudflare.com/generate_204`300,,50' "$file"
 
     echo "✅ 文件处理完成: $file"
 done
