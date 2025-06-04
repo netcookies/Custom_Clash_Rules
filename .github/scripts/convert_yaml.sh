b8201e49ccf7d351b13456b9874c5e547f5d4a5f
Isulew <224964+netcookies@users.noreply.github.com>
2025-06-04 16:48:12 +0800

    Update convert_yaml.sh

--- .github/scripts/convert_yaml.sh
+++ .github/scripts/convert_yaml.sh
@@ -55,9 +55,7 @@ find "$CFG_DIR" -type f -name "*.ini" | while read -r file; do
                 echo "    type: url-test" >> "$yaml_file"
                 echo "    include-all: true" >> "$yaml_file"
                 if [[ "$name" == "🌐 其他地区" ]]; then
-                    exclude_filter_step1=$(echo "$line" | grep -oP '\`\(.*\)\`' | sed 's/^`(//' | sed 's/)$//')
-                    echo "testing:*******$exclude_filter_step1"
-                    exclude_filter=$(echo "$exclude_filter_step1" | grep -oP '\(\K[^)]*(?=\))' | sed 's/^/(?i)/')
+                    exclude_filter=$(echo "$line" | grep -oP '\`\(.*\)\`' | sed -E 's/^\`\(\^\(\?!\.\*\((.*)\)\)\.\*\)\`$/\1/' | sed 's/^/(?i)/')
                     [[ -n "$exclude_filter" ]] && echo "    exclude-filter: $exclude_filter" >> "$yaml_file"
                 else
                     [[ -n "$filter" ]] && echo "    filter: $filter" >> "$yaml_file"
